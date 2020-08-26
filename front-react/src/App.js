4cb8f9d06c1c692452559e51749c4bd08869e028
Diego <diego_gomezpolo@outlook.com>
2020-08-26 12:37:15 -0500

    react shit

---
+++ front-react/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import logo from './logo.svg';
+import './App.css';
+import {Button} from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button>
+          Holaa
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
