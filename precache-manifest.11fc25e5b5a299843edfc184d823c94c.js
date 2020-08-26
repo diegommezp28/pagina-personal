ce4e7a3f5eae51055b564c0f897679cdbf6d8326
Diego <diego_gomezpolo@outlook.com>
2020-08-26 12:38:57 -0500

    Updates

---
+++ precache-manifest.11fc25e5b5a299843edfc184d823c94c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eeb01c94fb152137327b6a6b658c18d",
+    "url": "/pagina-personal/front-react/index.html"
+  },
+  {
+    "revision": "c137914d4868f84cb682",
+    "url": "/pagina-personal/front-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8fa361e5206e1db52521",
+    "url": "/pagina-personal/front-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c137914d4868f84cb682",
+    "url": "/pagina-personal/front-react/static/js/2.17d33dbe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pagina-personal/front-react/static/js/2.17d33dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa361e5206e1db52521",
+    "url": "/pagina-personal/front-react/static/js/main.9f0224c9.chunk.js"
+  },
+  {
+    "revision": "f8b7efe2cfcdfbff8412",
+    "url": "/pagina-personal/front-react/static/js/runtime-main.fa680399.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pagina-personal/front-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
