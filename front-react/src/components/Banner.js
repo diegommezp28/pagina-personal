8f2ffcdfa8b04196abcdd7502b331b0a8b202fae
Diego <diego_gomezpolo@outlook.com>
2020-08-26 23:40:08 -0500

    banner small devices

--- front-react/src/components/Banner.js
+++ front-react/src/components/Banner.js
@@ -5,10 +5,10 @@ import { Row, Col } from "react-bootstrap";
 const Banner = () => {
     return (
         <Row className="justify-content-md-center  banner-row">
-            <Col xs="12" md="6" className="banner-col">
+            <Col xs="12" md="6" className="banner-col mr-5">
                 <div className="typewritter-wrapper">
                     <div className="typewriter">
-                        <h2>Hello! I am Diego Andrés Gómez Polo</h2>
+                        <p>I am Diego Gómez Polo</p>
                     </div>
 
                 </div>
